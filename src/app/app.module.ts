b8b83d94bb30d7c475ebe57d99e98d323be35065
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-01-20 14:23:12 +0100

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { UserListComponent } from './components/user-list/user-list.component';
 import { LearningListComponent } from './components/learning-list/learning-list.component';
 import { MainComponent } from './components/main/main.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
 
 const mat = [
   MatCardModule,
+  MatIconModule
 ];
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAa_45DztHO0cemyoECFbBe_Bm_WGS6vV0",
+  authDomain: "stube-2d651.firebaseapp.com",
+  databaseURL: 'https://stube-2d651-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: "stube-2d651",
+  storageBucket: "stube-2d651.appspot.com",
+  messagingSenderId: "40376188203",
+  appId: "1:40376188203:web:808d89da57f2e2fc6b596e"
+};
 
 @NgModule({
   declarations: [
@@ -22,6 +37,9 @@ const mat = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule,
     ...mat
   ],
   exports: [ ...mat ],
