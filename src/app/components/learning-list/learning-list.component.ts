fcdf25c9abb704a241e4edcae306385b1335249a
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-01-23 00:07:42 +0100

    Update components to use generic list component

--- src/app/components/learning-list/learning-list.component.ts
+++ src/app/components/learning-list/learning-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgxSmartModalService } from 'ngx-smart-modal';
+import { DataService } from 'src/app/services/data.service';
+export interface Learning {
+  name: string, 
+  archive: boolean,
+}
 @Component({
   selector: 'app-learning-list',
   templateUrl: './learning-list.component.html',
   styleUrls: ['./learning-list.component.scss']
 })
 export class LearningListComponent implements OnInit {
+  learnings: Array<Learning> | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dataService: DataService, public ngxSmartModalService: NgxSmartModalService) {
   }
 
+  ngOnInit(): void {
+    this.dataService.getLearnings()
+      .subscribe((r: Learning[]) =>  this.learnings = r); 
+  }
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
-import { PageEvent } from "@angular/material/paginator";
-
+import { DataService } from 'src/app/services/data.service';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 export interface User {
   avatar: string, 
   name: string, 
   email: string,
 }
-
 
 @Component({
   selector: 'app-user-list',
@@ -17,36 +15,16 @@ export interface User {
 })
 export class UserListComponent implements OnInit {
   users: Array<User> | undefined;
-  filteredData: Array<User> | undefined;
-  searchTerm!: string;
 
-  lowValue: number = 0;
-  highValue: number = 20;
-  pageSize = 10;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
-
-  constructor(private userService: UserService) {
-  }
-
-
-  // used to build a slice of papers relevant at any given time
-  public getPaginatorData(event: PageEvent): PageEvent {
-    this.lowValue = event.pageIndex * event.pageSize;
-    this.highValue = this.lowValue + event.pageSize;
-    return event;
+  constructor(private dataService: DataService, public ngxSmartModalService: NgxSmartModalService) {
   }
 
   ngOnInit(): void {
-    this.userService.getUsers()
+    this.dataService.getUsers()
       .subscribe((r: User[]) =>  this.users = r); 
   }
 
-  search(value: string): void {
-    this.filteredData = this.users?.filter((val) => val.name.toLowerCase().includes(value));
-  }
-
   delete(data: User) {
-    this.userService.deleteUser(data);
+    this.dataService.deleteUser(data);
   }
-
 }
