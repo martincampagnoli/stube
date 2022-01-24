1c49025ca69a91f3e92da72c760b1397ad9e97c6
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-01-24 15:07:23 +0100

    Add delete on cascade

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { User } from '../models/User';
 
 @Injectable({
     providedIn: 'root'
@@ -26,10 +27,29 @@ export class DataService {
         const itemsRef = this.db.list('learnings');
         const key = data.id - 1;
         itemsRef.remove(key.toString());
+        this.removeLearningRef(data.id);
+    }
+
+    removeLearningRef(id: number) {
+        const userRef = this.db.list('users');
+        this.getUsers().subscribe((r: User[]) => {
+            r.forEach(elem => {
+                if (elem.learnings?.includes(id)) {
+                    elem.learnings.splice(elem.learnings.indexOf(id), 1); 
+                    this.updateUser(elem);
+                }
+            });
+        });
     }
 
     updateLearning(data: any): void {
         const itemsRef = this.db.list('learnings');
+        const key = data.id - 1;
+        itemsRef.set(key.toString(), data);
+    }
+
+    updateUser(data: any){
+        const itemsRef = this.db.list('users');
         const key = data.id - 1;
         itemsRef.set(key.toString(), data);
     }
